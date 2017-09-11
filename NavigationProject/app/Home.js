dfa9f5984091ee7ef2ca609b625eaf5cbc5f9a5b
Cristiane Ferreira <cris.mayara@gmail.com>
2017-09-11 15:44:12 -0300

    Adding a button on the Home screen

--- NavigationProject/app/Home.js
+++ NavigationProject/app/Home.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+//import { StackNavigator } from 'react-navigation';
 
 class Home extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
+
+  _onPressButton = () => {
+
+  }
+
   render() {
     return (
       <View>
         <Text>
           Welcome home!
         </Text>
+        <Button
+          onPress={this._onPressButton}
+          title="Next"
+          color="#841584"
+        />
       </View>
     );
   }
