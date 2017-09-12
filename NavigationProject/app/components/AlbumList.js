2161461b6586415368efa4ae177de226b7b0c14c
Cristiane Ferreira <cris.mayara@gmail.com>
2017-09-12 15:42:49 -0300

    Adding the AlbumDetail component

--- NavigationProject/app/components/AlbumList.js
+++ NavigationProject/app/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail'
 import axios from 'axios';
 
 class AlbumList extends Component {
@@ -12,7 +13,7 @@ class AlbumList extends Component {
 
     renderAlbums() {
         return this.state.albums.map(album => 
-            <Text>{album.title}</Text>
+            <AlbumDetail key={album.title} album={album} />
         );
     }
 
