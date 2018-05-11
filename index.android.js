6a572b3affe6c8aca6544fe1fbf42eeefa196717
Cristiane Mayara <cris.mayara@gmail.com>
2018-05-11 14:56:02 -0300

    Altering the index file for both app versions

--- index.android.js
+++ index.android.js
@@ -1,21 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import App from './app/index';
 
-class Albums extends Component {
-  render() {
-    return (
-      <App />
-    );
-  }
-}
-
-export default Albums;
-
-AppRegistry.registerComponent('Albums', () => Albums);
+AppRegistry.registerComponent('Albums', () => App);
